6e1b1b3bcfc4dc285006eaf6483221809a0eca80
gremyakoN <lorena@tut.by>
2019-10-04 14:18:44 +0300

    добавили строку

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setTitle("внесли изменения");
+        primaryStage.setTitle("добавили строчку2");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
